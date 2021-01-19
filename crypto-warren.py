9194555a563a0787f41700d144a2f6ebed5590c6
Tim Grob <tim.grob@six-group.com>
2021-01-19 14:02:22 +0100

    infinite loop added

--- crypto-warren.py
+++ crypto-warren.py
@@ -4,6 +4,7 @@ from os import environ
 
 if __name__ == "__main__":
     key = environ['test-key']
-    print('hello world')
-    print(key)
-    print('----------')
+    while True:
+        print('hello world')
+        print(key)
+        print('----------')
