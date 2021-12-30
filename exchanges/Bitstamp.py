b760229f60ac96a86b6893e5d8ed092ccbc17bd4
Grob <tim.grob@six-group.com>
2021-12-30 17:59:13 +0100

    new exchanges and framework for exchanges added

---
+++ exchanges/Bitstamp.py
@@ -0,0 +1,21 @@
+import ccxt
+from exchanges.Exchange import Exchange
+
+
+class Bitstamp(Exchange):
+
+    def connect(self):
+        self.exchange = ccxt.bitstamp({
+            'apiKey': self.api_key,
+            'secret': self.secret,
+            'timeout': self.time_out,
+            'enableRateLimit': self.enable_rate_limit
+        })
+
+    def fetch_balance(self):
+        balance_data = self.exchange.fetch_balance()
+        return balance_data
+
+    def fetch_token_data(self, token: str):
+        current_ticker = self.exchange.fetch_ticker(token)
+        return current_ticker
---
+++ exchanges/Exchange.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+import json
+
+
+class Exchange(ABC):
+    """This is the blueprint for an exchange"""
+
+    def __init__(self, api_key: str, secret: str, time_out: int = 3000, enable_rate_limit: bool = True):
+        self.api_key = api_key
+        self.secret = secret
+        self.time_out = time_out
+        self.enable_rate_limit = enable_rate_limit
+        self.exchange = None
+
+    @abstractmethod
+    def connect(self) -> None:
+        """Establish connection to exchange"""
+
+    @abstractmethod
+    def fetch_balance(self) -> json:
+        """Retrieve balance data of the exchange"""
+
+    @abstractmethod
+    def fetch_token_data(self, token: str) -> json:
+        """Retrieve data for a particular coin"""
