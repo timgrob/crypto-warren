6551662759e31ea10afcff7152b3f0a6ee470f1b
tk4gt <tim.grob@six-group.com>
2022-01-16 11:11:28 +0100

    Trader bot for volatility trading updated

--- bots/TradingBot.py
+++ bots/TradingBot.py
@@ -1,12 +1,14 @@
 from abc import ABC, abstractmethod
 from exchanges import Exchange
+from strategies.TradingStrategy import TradingStrategy
 
 
 class TradingBot(ABC):
     """This is a blueprint for different trading bots"""
 
-    def __init__(self, exchange: Exchange) -> None:
+    def __init__(self, exchange: Exchange, trading_strategy: TradingStrategy) -> None:
         self.exchange = exchange
+        self.trading_strategy = trading_strategy
 
     @abstractmethod
     def trade(self, token: str) -> None:
--- bots/VolatilityTrader.py
+++ bots/VolatilityTrader.py
@@ -1,27 +1,29 @@
+import os
 import time
 import random
+import ccxt
 import logging
-import sched
 from queue import LifoQueue
 from datetime import datetime, timedelta
 from strategies.Trade import Trade
 from bots.TradingBot import TradingBot
-from exchanges.Exchange import Exchange
+from strategies.TradingStrategy import TradingStrategy
 
 
 class VolatilityTrader(TradingBot):
 
-    def __init__(self, exchange: Exchange, margin: float = 0.1, max_number_investment: int = 4) -> None:
-        super().__init__(exchange)
-        self.margin = margin
+    def __init__(self, exchange: ccxt.Exchange, trading_strategy: TradingStrategy, max_number_investment: int = 4) -> None:
+        super().__init__(exchange, trading_strategy)
         self.trades = LifoQueue(max_number_investment)
         self.max_number_investment = max_number_investment
+        self.RUNNING = True
 
     def trade(self, token: str) -> None:
+        """This is the trading function for the volatility trader"""
+
         # initialize logger
-        logging.basicConfig(filename='logfile.log', level=logging.INFO)
-        logging.info(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: Start Program")
-        print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: Start Program")
+        logfile_path = os.path.join(os.path.dirname(__file__), os.pardir)
+        logging.basicConfig(filename=os.path.join(logfile_path, 'logfile.log'), level=logging.INFO)
 
         currencies = token.split('/')
         balance = self.exchange.fetch_balance()
@@ -29,26 +31,40 @@ class VolatilityTrader(TradingBot):
         invest_cash_amount = total_cash/self.max_number_investment
         last_buy_trade = Trade((datetime.now() - timedelta(hours=24)), token, 0, 0.0)
 
-        print(f"Trade on {token} and margin {self.margin} with an investment amount of {invest_cash_amount}")
+        info_txt = f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: " \
+                   f"Trading {token} - {self.trading_strategy} with {invest_cash_amount}{currencies[1]}"
+        logging.info(info_txt)
 
-        while True:
-            token_data = self.exchange.fetch_token_data(token)
-            passed_24h = True if (datetime.now() - timedelta(hours=24)) > last_buy_trade.timestamp else False
+        while self.RUNNING:
+            token_data = self.exchange.fetch_ticker(token)
+            passed_24h = True if (datetime.now() - timedelta(hours=12)) > last_buy_trade.timestamp else False
 
-            if token_data['bid'] >= (1 + (self.margin*0.6)) * token_data['low'] and not self.trades.empty() and token_data['bid'] > last_buy_trade.price:
-                last_trade = self.trades.get()
-                sell_order = self.exchange.create_limit_sell_order(token_data['symbol'], last_trade.qty, token_data['bid'])
+            if self.trading_strategy.sell_signal(token_data) and not self.trades.empty() and token_data['bid'] > last_buy_trade.price:
+                last_buy_trade = self.trades.get()
+                sell_order = self.exchange.create_limit_sell_order(token_data['symbol'], last_buy_trade.qty, token_data['bid'])
+
+                # update last buy trade, yet remember to put the element back into the queue
+                if not self.trades.empty():
+                    last_buy_trade = self.trades.get()
+                    self.trades.put(last_buy_trade)
 
                 # log sell trade
                 info_txt_sell = f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: "\
                                 f"{sell_order['side']} {sell_order['symbol']} - "\
                                 f"{sell_order['amount']} @ {sell_order['price']}"
                 logging.info(info_txt_sell)
-                print(info_txt_sell)
-            elif token_data['ask'] <= (1 - self.margin) * token_data['high'] and not self.trades.full() and passed_24h:
-                amount = invest_cash_amount/token_data['ask']
+
+            elif self.trading_strategy.buy_signal(token_data) and not self.trades.full() and passed_24h:
+                amount = round(invest_cash_amount/token_data['ask'], 4)
                 buy_order = self.exchange.create_limit_buy_order(token_data['symbol'], amount, token_data['ask'])
-                last_buy_trade = Trade(datetime.strptime(buy_order['datetime'].split('.')[0], '%Y-%m-%dT%H:%M:%S'),
+
+                # not all exchanges return a datetime value, because the trade may not have instantly been executed
+                if buy_order['datetime']:
+                    buy_oder_datetime = datetime.strptime(buy_order['datetime'], '%Y-%m-%dT%H:%M:%S.%fZ')
+                else:
+                    buy_oder_datetime = datetime.now()
+
+                last_buy_trade = Trade(buy_oder_datetime,
                                        buy_order['symbol'],
                                        buy_order['amount'],
                                        buy_order['price']
@@ -60,8 +76,7 @@ class VolatilityTrader(TradingBot):
                                f"{buy_order['side']} {buy_order['symbol']} - "\
                                f"{buy_order['amount']} @ {buy_order['price']}"
                 logging.info(info_txt_buy)
-                print(info_txt_buy)
             else:
-                time.sleep(random.randint(30, 120))
+                time.sleep(random.randint(1, 2))
                 print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: continue")
                 continue
