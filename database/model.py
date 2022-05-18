76b4ed6296f237f1671eba67603e516d6faf4da5
tk4gt <tim.grob@six-group.com>
2022-05-18 15:12:15 +0200

    renamed files

---
+++ database/model.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, Float, String, Sequence, DateTime
+from sqlalchemy.orm import declarative_base
+
+
+Base = declarative_base()
+
+
+class Trade(Base):
+    __tablename__ = 'trades'
+
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    timestamp = Column(DateTime)
+    symbol = Column(String)
+    qty = Column(Float)
+    price = Column(Float)
+
+    def __repr__(self):
+        return f"<Trade(TimeStamp={self.timestamp}, symbol={self.symbol}, quantity={self.qty}, price={self.price})>"
+
+Base.metadata.create_all(engine)
